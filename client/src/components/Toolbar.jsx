d9b8d2988c090bc0f3e2f5054537b2682ccefc21
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-29 01:05:57 +0300

    feat: add buttons to toolbar

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -4,7 +4,10 @@ import '../styles/toolbar.scss';
 const Toolbar = () => {
     return (
         <div className="toolbar">
-            
+            <button className="toolbar__btn brush"></button>
+            <button className="toolbar__btn rect"></button>
+            <button className="toolbar__btn circle"></button>
+            <button className="toolbar__btn eraser"></button>
         </div>
     );
 };
