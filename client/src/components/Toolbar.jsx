ef0f37f898a95e010c113c38eee895d624dc5a0a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-29 01:42:13 +0300

    feat: set tool by button

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/toolbar.scss';
+import toolState from "../store/toolState.js";
+import Brush from "../tools/Brush.js";
+import canvasState from "../store/canvasState.js";
 
 const Toolbar = () => {
     return (
         <div className="toolbar">
-            <button className="toolbar__btn brush"></button>
+            <button className="toolbar__btn brush"
+                    onClick={() => toolState.setTool(new Brush(canvasState.canvas))}>
+            </button>
             <button className="toolbar__btn rect"></button>
             <button className="toolbar__btn circle"></button>
             <button className="toolbar__btn eraser"></button>
