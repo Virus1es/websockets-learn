455e6f41faadc172d4e3f3c7315d79687556d9ff
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-29 01:54:40 +0300

    fix: fix rect draw

--- client/src/tools/Rect.js
+++ client/src/tools/Rect.js
@@ -21,6 +21,7 @@ export default class Rect extends Tool {
         this.ctx.beginPath();
         this.startX = e.pageX - e.target.offsetLeft;
         this.startY = e.pageY - e.target.offsetTop;
+        this.saved = this.canvas.toDataURL();
     }
 
     mouseMoveHandler(e) {
@@ -34,8 +35,15 @@ export default class Rect extends Tool {
     }
 
     draw(x, y, w, h) {
-        this.ctx.rect(x, y, w, h);
-        this.ctx.fill();
-        this.ctx.stroke();
+        const img = new Image();
+        img.src = this.saved;
+        img.onload = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.beginPath();
+            this.ctx.rect(x, y, w, h);
+            this.ctx.fill();
+            this.ctx.stroke();
+        };
     }
 }
