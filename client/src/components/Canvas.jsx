61e2e6cbd9b3f63e900208cbf61da673448c0c68
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-29 01:24:36 +0300

    feat: use mobx to remember canvas tag

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import '../styles/canvas.scss'
+import {observer} from "mobx-react-lite";
+import canvasState from "../store/canvasState.js";
 
-const Canvas = () => {
+const Canvas = observer (() => {
+
+    const canvasRef = useRef(null);
+
+
+    useEffect(() => {
+        canvasState.setCanvas(canvasRef.current);
+    }, [canvasRef]);
+
     return (
         <div className="canvas">
-            <canvas width={600} height={400}/>
+            <canvas ref={canvasRef} width={600} height={400}/>
         </div>
     );
-};
+});
 
 export default Canvas;
