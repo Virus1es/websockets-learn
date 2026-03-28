3544c21aa2a99ec624214e7b171b1c5a24edb4fb
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-29 00:51:20 +0300

    feat: use a components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import './styles/app.scss'
+import Toolbar from "./components/Toolbar.jsx";
+import SettingBar from "./components/SettingBar.jsx";
+import Canvas from "./components/Canvas.jsx";
 
 function App() {
 
-  return (
-    <div className="app">
-
-    </div>
-  )
+    return (
+        <div className="app">
+            <Toolbar/>
+            <SettingBar/>
+            <Canvas/>
+        </div>
+    )
 }
 
 export default App
