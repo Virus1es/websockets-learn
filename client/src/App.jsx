eba93ed502f8e642af33296d11b066ec54beeb22
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-30 00:30:33 +0300

    feat: add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,27 @@ import './styles/app.scss'
 import Toolbar from "./components/Toolbar.jsx";
 import SettingBar from "./components/SettingBar.jsx";
 import Canvas from "./components/Canvas.jsx";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
 
     return (
-        <div className="app">
-            <Toolbar/>
-            <SettingBar/>
-            <Canvas/>
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <Routes>
+                    <Route
+                        path='/:id'
+                        element={
+                            <>
+                                <Toolbar/>
+                                <SettingBar/>
+                                <Canvas/>
+                            </>
+                    }/>
+                </Routes>
+                <Navigate to={`f${(+new Date).toString(16)}`} replace/>
+            </div>
+        </BrowserRouter>
     )
 }
 
