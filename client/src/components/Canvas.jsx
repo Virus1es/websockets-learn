d943aaa63be7c321d428c1f02ec7eac7ffbbc751
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-30 00:38:54 +0300

    feat: connect user to app

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import '../styles/canvas.scss'
 import {observer} from "mobx-react-lite";
 import canvasState from "../store/canvasState.js";
 import toolState from "../store/toolState.js";
 import Brush from "../tools/Brush.js";
 import {Button, Modal} from "react-bootstrap";
+import {useParams} from "react-router-dom";
 
 const Canvas = observer (() => {
 
@@ -12,10 +13,31 @@ const Canvas = observer (() => {
 
     const usernameRef = useRef(null);
 
+    const [modal, setModal] = useState(true);
+
+    const params = useParams();
+
     useEffect(() => {
         canvasState.setCanvas(canvasRef.current);
         toolState.setTool(new Brush(canvasRef.current));
     }, []);
+
+    useEffect(() => {
+        if(canvasState.username) {
+            const socket = new WebSocket('ws://localhost:5000/');
+            socket.onopen = () => {
+                console.log('Connection set');
+                socket.send(JSON.stringify({
+                    id: params.id,
+                    username: canvasState.username,
+                    method: "connection"
+                }))
+            }
+            socket.onmessage = (event) => {
+                console.log(event.data);
+            }
+        }
+    }, [canvasState.username]);
 
     const mouseDownHandler = () => {
         canvasState.pushToUndo(canvasRef.current.toDataURL());
@@ -23,11 +45,12 @@ const Canvas = observer (() => {
 
     const connectionHandler = () => {
         canvasState.setUsername(usernameRef.current.value);
+        setModal(false);
     }
 
     return (
         <div className="canvas">
-            <Modal show={true} onHide={() => {}}>
+            <Modal show={modal} onHide={() => {}}>
                 <Modal.Header closeButton>
                     <Modal.Title>Введите ваше имя</Modal.Title>
                 </Modal.Header>
