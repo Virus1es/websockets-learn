01bf04415c1bb670ccdef3395ecbecd52065fab4
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-29 22:44:30 +0300

    feat: fill setting bar

--- client/src/components/SettingBar.jsx
+++ client/src/components/SettingBar.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/toolbar.scss';
 
 const SettingBar = () => {
+    const [lineWidth, setLineWidth] = useState(1);
+
     return (
         <div className="setting-bar">
-
+            <p>Толщина линии:</p>
+            <input type="number"
+                   value={lineWidth}
+                   onChange={(e) => setLineWidth(Number(e.target.value))}
+                   style={{width: '3%'}}
+                   min={1}
+                   max={20}
+            />
+            <p>Цвет обводки</p>
+            <input type="color" />
         </div>
     );
 };
