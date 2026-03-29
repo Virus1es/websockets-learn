2539a6c5767fb51def21327395f7721c195e0b93
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-30 00:30:04 +0300

    feat: add endpoints to server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,35 @@ const app = express();
 
 const WSServer = require('express-ws')(app);
 
+const aWss = WSServer.getWss();
+
 const PORT = process.env.PORT ?? 5000;
 
+app.ws('/', (ws, req) => {
+    console.log('CONNECTED');
+    ws.send('Connect are success')
+    ws.on('message', (msg) => {
+        msg = JSON.parse(msg);
+        switch (msg.method) {
+            case 'connection':
+                break;
+            case 'message':
+                break;
+        }
+    });
+});
+
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+
+const connectionHandler = (ws, msg) => {
+    ws.id = msg.id;
+    broadcastConnection(ws, msg);
+}
+
+const broadcastConnection = (ws, msg) => {
+    aWss.clients.forEach(client => {
+        if(client.id === msg.id) {
+            client.send(`User ${msg.username} connected`);
+        }
+    })
+}
