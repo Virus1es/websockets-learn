f46507f059a85155f139f04712d71d9291c8dad5
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-30 00:30:20 +0300

    feat: add Modal to app

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import {observer} from "mobx-react-lite";
 import canvasState from "../store/canvasState.js";
 import toolState from "../store/toolState.js";
 import Brush from "../tools/Brush.js";
+import {Button, Modal} from "react-bootstrap";
 
 const Canvas = observer (() => {
 
@@ -19,8 +20,25 @@ const Canvas = observer (() => {
         canvasState.pushToUndo(canvasRef.current.toDataURL());
     };
 
+    const connectionHandler = () => {
+
+    }
+
     return (
         <div className="canvas">
+            <Modal show={true} onHide={() => {}}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Введите ваше имя</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <input type="text"/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => connectionHandler()}>
+                        Войти
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <canvas
                 ref={canvasRef}
                 width={600}
