9ea3c6fd18cafbcc126b00c41ed561b40a375839
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-31 23:34:18 +0300

    feat: add endpoints to sync users paint fields

--- server/index.js
+++ server/index.js
@@ -6,7 +6,16 @@ const WSServer = require('express-ws')(app);
 
 const aWss = WSServer.getWss();
 
+const cors = require('cors');
+
+const fs = require('fs');
+
+const path = require('path');
+
 const PORT = process.env.PORT ?? 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.ws('/', (ws, req) => {
     console.log('CONNECTED');
@@ -21,6 +30,25 @@ app.ws('/', (ws, req) => {
                 break;
         }
     });
+});
+
+app.post('/image', (req, res) => {
+    try{
+        const data = req.body.img.replace('data:image/png;base64,', '');
+        fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64');
+        return res.status(200).json({message: "Загружено"})
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Server error');
+    }
+});
+app.get('/image', (req, res) => {
+    try{
+
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('Server error');
+    }
 });
 
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
